e464f42340cdcaf7e45cba91a1e318bf5cf77640
baca217 <baca217@gmail.com>
2021-04-29 20:29:08 -0600

    added more tests for local music player and youtube music

---
+++ tests/hs103_smartplug/hs103_sp_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('tools')
+sys.path.append('modules')
+import sklearn_sims as sk
+import module_loader as ml
+import pathlib
+import time
+
+def main():
+    classes = ml.class_builder()
+    info = {"path" : str(pathlib.Path(__file__).parent.absolute())}
+    voice = None
+    commands = [
+            "turn off the smart plug",
+            "turn on the smart plug",
+            "get the status of the smart plug",
+            ]
+
+    for i in commands:
+        print(i)
+        msg, func, mod = sk.compare_command(i, classes, info)
+        run_results(msg, func, mod, classes, voice)
+
+def run_results(msg, func, mod, classes, voice):
+    print(msg)
+    if func: #we got a func back
+        if mod in classes.keys(): #classes funcs should manipulate themselves
+            func(classes[mod])
+        else:
+            func()
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main()
